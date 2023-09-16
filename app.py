22ebaf1e5b08f3dc4b54f7382eb85380a524a91c
rohansingh-chat360 <rohan.singh@chat360.io>
2023-09-17 00:03:51 +0530

    raid session

---
+++ app.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from inference import load, predict
+import uvicorn
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["GET", "POST", "OPTIONS"],  # Add the allowed HTTP methods
+    allow_headers=["*"]  # Add the allowed headers
+)
+
+class QueryInput(BaseModel):
+    query: str
+
+# Define the directory where the model is saved
+model_path = 'models/text_classification_model.h5'
+tokenizer_path = 'tokenizer/tokenizer.pkl'
+max_sequence_path = 'tokenizer/max_sequence_length.txt'
+
+model, tokenizer, max_len = load(model_path, tokenizer_path, max_sequence_path)
+
+@app.get("/ping")
+async def ping():
+    return "Hello"
+
+@app.post("/predict")
+async def query(query_input: QueryInput):
+    # Hardcoded prediction for demonstration purposes.
+    prediction = predict(query_input.query, model, tokenizer, max_len)
+    return {"prediction": prediction}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
